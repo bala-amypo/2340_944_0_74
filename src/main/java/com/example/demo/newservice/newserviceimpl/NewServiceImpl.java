eccd6168779778b3cf31f1a1b225735c917e4d84
bala-amypo <bala@amypo.in>
2025-12-17 09:29:53 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
@@ -6,9 +6,12 @@ import com.example.demo.newservice.NewfileService;
 @Service
 public class NewfileServiceImpl implements NewfileService{
  private final NewfileRepo rep;
+ public NewfileServiceImpl(NewfileRepo rep){
+    this.rep=rep;
+ }
  @Override
  public NewfileEntity savedata(NewfileEntity newfile) {
-
+           return rep.save(newfile);
  }
  @Override
  public NewfileEntity getidval(Long id) {
