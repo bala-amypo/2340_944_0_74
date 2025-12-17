6ff3ed40226a0e6788de14377b7161a28f4d8086
bala-amypo <bala@amypo.in>
2025-12-17 08:14:53 +0000

    User commit

--- src/main/java/com/example/demo/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/newentity/NewfileEntity.java
@@ -2,7 +2,7 @@ package com.example.demo.newentity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-
+@Entity
 public class NewfileEntity{
 @Id
 @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -12,4 +12,28 @@ private String name;
 @NotBlank(message = "No blank allowed")
 @Email(message = "Invalid Format")
 private String email;
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public String getEmail() {
+    return email;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+public NewfileEntity(Long id, @NotBlank(message = "should not contain spaces") String name,
+        @NotBlank(message = "No blank allowed") @Email(message = "Invalid Format") String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+}
 }
